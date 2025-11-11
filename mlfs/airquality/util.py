6b2860b7c9df926847a0ad249401fb1e0232fa7e
David Bazaldua <david.bazaldua13@gmail.com>
2025-11-11 23:06:33 +0100

    more par

--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -20,29 +20,28 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
 
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-    retry_session = retry(cache_session, retries = 9, backoff_factor = 0.2)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
     openmeteo = openmeteo_requests.Client(session = retry_session)
 
     # Make sure all required weather variables are listed here
     # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "start_date": start_date,
-        "end_date": end_date,
-        "daily": ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max", "wind_direction_10m_dominant", "daylight_duration", "precipitation_hours", "sunshine_duration"],
+	    "latitude": 40.4165,
+	    "longitude": -3.7026,
+	    "start_date": "2025-10-26",
+	    "end_date": "2025-11-09",
+	    "daily": ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max", "wind_direction_10m_dominant", "daylight_duration", "precipitation_hours", "sunshine_duration"],
     }
     responses = openmeteo.weather_api(url, params=params)
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation: {response.Elevation()} m asl")
+    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-    # Process daily data. The order of variables needs to be the same as requested.
+# Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
     daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
     daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
@@ -53,11 +52,12 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     daily_sunshine_duration = daily.Variables(6).ValuesAsNumpy()
 
     daily_data = {"date": pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit = "s"),
-        end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
-        freq = pd.Timedelta(seconds = daily.Interval()),
-        inclusive = "left"
+	    start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+	    end =  pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = daily.Interval()),
+	    inclusive = "left"
     )}
+
     daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
     daily_data["precipitation_sum"] = daily_precipitation_sum
     daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
@@ -65,7 +65,6 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     daily_data["daylight_duration"] = daily_daylight_duration
     daily_data["precipitation_hours"] = daily_precipitation_hours
     daily_data["sunshine_duration"] = daily_sunshine_duration
-
     daily_dataframe = pd.DataFrame(data = daily_data)
     daily_dataframe = daily_dataframe.dropna()
     daily_dataframe['city'] = city
