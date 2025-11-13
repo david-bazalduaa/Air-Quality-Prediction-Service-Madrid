a83f4cf7f6b4a71a9d067a64408f2ee1c462afe9
David Bazaldua <david.bazaldua13@gmail.com>
2025-11-13 22:55:57 +0100

    change util parameters

--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -27,11 +27,11 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "start_date": start_date,
-        "end_date": end_date,
-        "daily": ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max", "wind_direction_10m_dominant", "daylight_duration", "precipitation_hours", "sunshine_duration"],
+        "latitude": 52.52,
+        "longitude": 13.41,
+        "start_date": "2025-10-28",
+        "end_date": "2025-11-11",
+        "daily": ["temperature_2m_mean", "precipitation_hours", "wind_speed_10m_max", "wind_direction_10m_dominant", "cloud_cover_max", "relative_humidity_2m_mean", "dew_point_2m_max"],
     }
     responses = openmeteo.weather_api(url, params=params)
 
@@ -44,12 +44,12 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     # Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
     daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-    daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
+    daily_precipitation_hours = daily.Variables(1).ValuesAsNumpy()
     daily_wind_speed_10m_max = daily.Variables(2).ValuesAsNumpy()
     daily_wind_direction_10m_dominant = daily.Variables(3).ValuesAsNumpy()
-    daily_daylight_duration = daily.Variables(4).ValuesAsNumpy()
-    daily_precipitation_hours = daily.Variables(5).ValuesAsNumpy()
-    daily_sunshine_duration = daily.Variables(6).ValuesAsNumpy()
+    daily_cloud_cover_max = daily.Variables(4).ValuesAsNumpy()
+    daily_relative_humidity_2m_mean = daily.Variables(5).ValuesAsNumpy()
+    daily_dew_point_2m_max = daily.Variables(6).ValuesAsNumpy()
 
     daily_data = {"date": pd.date_range(
         start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
@@ -59,12 +59,12 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     )}
 
     daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
-    daily_data["precipitation_sum"] = daily_precipitation_sum
+    daily_data["precipitation_hours"] = daily_precipitation_hours
     daily_data["wind_speed_10m_max"] = daily_wind_speed_10m_max
     daily_data["wind_direction_10m_dominant"] = daily_wind_direction_10m_dominant
-    daily_data["daylight_duration"] = daily_daylight_duration
-    daily_data["precipitation_hours"] = daily_precipitation_hours
-    daily_data["sunshine_duration"] = daily_sunshine_duration
+    daily_data["cloud_cover_max"] = daily_cloud_cover_max
+    daily_data["relative_humidity_2m_mean"] = daily_relative_humidity_2m_mean
+    daily_data["dew_point_2m_max"] = daily_dew_point_2m_max
 
     daily_dataframe = pd.DataFrame(data = daily_data)
     daily_dataframe = daily_dataframe.dropna()
@@ -76,7 +76,7 @@ def get_hourly_weather_forecast(city, latitude, longitude):
     # latitude, longitude = get_city_coordinates(city)
 
     # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
     openmeteo = openmeteo_requests.Client(session = retry_session)
 
@@ -84,48 +84,50 @@ def get_hourly_weather_forecast(city, latitude, longitude):
     # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "hourly": ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max", "wind_direction_10m_dominant", "daylight_duration", "precipitation_hours", "sunshine_duration"],
+        "latitude": 52.52,
+        "longitude": 13.41,
+        "start_date": "2025-10-28",
+        "end_date": "2025-11-11",
+        "hourly": ["", "temperature_2m", "precipitation", "wind_speed_10m", "wind_direction_10m", "cloud_cover", "relative_humidity_2m", "dew_point_2m"],
     }
     responses = openmeteo.weather_api(url, params=params)
 
     # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+    print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation: {response.Elevation()} m asl")
+    print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
     # Process hourly data. The order of variables needs to be the same as requested.
-
     hourly = response.Hourly()
-    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-    hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
-    hourly_wind_speed_10m = hourly.Variables(2).ValuesAsNumpy()
-    hourly_wind_direction_10m = hourly.Variables(3).ValuesAsNumpy()
-    hourly_daylight_duration = hourly.Variables(4).ValuesAsNumpy()
-    hourly_precipitation_hours = hourly.Variables(5).ValuesAsNumpy()
-    hourly_sunshine_duration = hourly.Variables(6).ValuesAsNumpy()
+    hourly_ = hourly.Variables(0).ValuesAsNumpy()
+    hourly_temperature_2m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(3).ValuesAsNumpy()
+    hourly_wind_direction_10m = hourly.Variables(4).ValuesAsNumpy()
+    hourly_cloud_cover = hourly.Variables(5).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(6).ValuesAsNumpy()
+    hourly_dew_point_2m = hourly.Variables(7).ValuesAsNumpy()
 
     hourly_data = {"date": pd.date_range(
-        start = pd.to_datetime(hourly.Time(), unit = "s"),
-        end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
         freq = pd.Timedelta(seconds = hourly.Interval()),
         inclusive = "left"
     )}
-    hourly_data["temperature_2m_mean"] = hourly_temperature_2m
-    hourly_data["precipitation_sum"] = hourly_precipitation
-    hourly_data["wind_speed_10m_max"] = hourly_wind_speed_10m
-    hourly_data["wind_direction_10m_dominant"] = hourly_wind_direction_10m
-    hourly_data["daylight_duration"] = hourly_daylight_duration
-    hourly_data["precipitation_hours"] = hourly_precipitation_hours
-    hourly_data["sunshine_duration"] = hourly_sunshine_duration
+
+    hourly_data[""] = hourly_
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["precipitation"] = hourly_precipitation
+    hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+    hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+    hourly_data["cloud_cover"] = hourly_cloud_cover
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["dew_point_2m"] = hourly_dew_point_2m
 
     hourly_dataframe = pd.DataFrame(data = hourly_data)
     hourly_dataframe = hourly_dataframe.dropna()
     return hourly_dataframe
-
 
 
 def get_city_coordinates(city_name: str):
