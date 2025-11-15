e80fe121bcd2ea47bb9a910d04e64a29ba3cdca1
David Bazaldua <david.bazaldua13@gmail.com>
2025-11-16 00:01:22 +0100

    multi-sensor-madrid-1-done

--- mlfs/airquality/util_2.py
+++ mlfs/airquality/util_2.py
@@ -15,48 +15,51 @@ import hopsworks
 import hsfs
 from pathlib import Path
 
-def get_historical_weather(city, start_date,  end_date, latitude, longitude):
-    # latitude, longitude = get_city_coordinates(city)
+def get_historical_weather_2(city, start_date, end_date, latitude, longitude):
+    cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
 
-    # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-    openmeteo = openmeteo_requests.Client(session = retry_session)
-
-    # Make sure all required weather variables are listed here
-    # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
         "latitude": latitude,
         "longitude": longitude,
         "start_date": start_date,
         "end_date": end_date,
-        "daily": ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max", "wind_direction_10m_dominant", "cloud_cover_max", "relative_humidity_2m_mean", "dew_point_2m_max"],
+        "daily": [
+            "temperature_2m_mean",
+            "precipitation_sum",
+            "wind_speed_10m_max",
+            "wind_direction_10m_dominant",
+            "cloud_cover_max",
+            "relative_humidity_2m_mean",
+            "dew_point_2m_max",
+        ],
     }
     responses = openmeteo.weather_api(url, params=params)
 
-    # Process first location. Add a for-loop for multiple locations or weather models
     response = responses[0]
     print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
     print(f"Elevation: {response.Elevation()} m asl")
     print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-    # Process daily data. The order of variables needs to be the same as requested.
     daily = response.Daily()
     daily_temperature_2m_mean = daily.Variables(0).ValuesAsNumpy()
-    daily_precipitation_hours = daily.Variables(1).ValuesAsNumpy()
+    daily_precipitation_sum = daily.Variables(1).ValuesAsNumpy()
     daily_wind_speed_10m_max = daily.Variables(2).ValuesAsNumpy()
     daily_wind_direction_10m_dominant = daily.Variables(3).ValuesAsNumpy()
     daily_cloud_cover_max = daily.Variables(4).ValuesAsNumpy()
     daily_relative_humidity_2m_mean = daily.Variables(5).ValuesAsNumpy()
     daily_dew_point_2m_max = daily.Variables(6).ValuesAsNumpy()
 
-    daily_data = {"date": pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
-        end =  pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
-        freq = pd.Timedelta(seconds = daily.Interval()),
-        inclusive = "left"
-    )}
+    daily_data = {
+        "date": pd.date_range(
+            start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+            end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+            freq=pd.Timedelta(seconds=daily.Interval()),
+            inclusive="left",
+        )
+    }
 
     daily_data["temperature_2m_mean"] = daily_temperature_2m_mean
     daily_data["precipitation_sum"] = daily_precipitation_sum
@@ -66,9 +69,10 @@ def get_historical_weather(city, start_date,  end_date, latitude, longitude):
     daily_data["relative_humidity_2m_mean"] = daily_relative_humidity_2m_mean
     daily_data["dew_point_2m_max"] = daily_dew_point_2m_max
 
-    daily_dataframe = pd.DataFrame(data = daily_data)
+    daily_dataframe = pd.DataFrame(data=daily_data)
     daily_dataframe = daily_dataframe.dropna()
-    daily_dataframe['city'] = city
+    daily_dataframe["city"] = city
+
     return daily_dataframe
 
 def get_hourly_weather_forecast(city, latitude, longitude):
