6c10157868776490b2286e6abca6fc1494c5ed17
David Bazaldua <david.bazaldua13@gmail.com>
2025-11-16 22:40:11 +0100

    commit all

--- mlfs/airquality/util_2.py
+++ mlfs/airquality/util_2.py
@@ -305,7 +305,7 @@ def backfill_predictions_for_monitoring(weather_fg, air_quality_df, monitor_fg, 
     features_df = weather_fg.read()
     features_df = features_df.sort_values(by=['date'], ascending=True)
     features_df = features_df.tail(10)
-    features_df['predicted_pm25'] = model.predict(features_df[['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant','cloud_cover_max', 'relative_humidity_2m_mean', 'dew_point_2m_max']])
+    features_df['predicted_pm25'] = model.predict(features_df[['weather_madrid_sensors_temperature_2m_mean', 'weather_madrid_sensors_precipitation_sum', 'weather_madrid_sensors_wind_speed_10m_max', 'weather_madrid_sensors_wind_direction_10m_dominant', 'weather_madrid_sensors_cloud_cover_max', 'weather_madrid_sensors_relative_humidity_2m_mean', 'weather_madrid_sensors_dew_point_2m_max']])
     df = pd.merge(features_df, air_quality_df[['date','pm25','street','country']], on="date")
     df['days_before_forecast_day'] = 1
     hindcast_df = df
